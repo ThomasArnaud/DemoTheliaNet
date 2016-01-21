692b43897230051b469839bf509d811baf483bd3
Thomas Arnaud <tarnaud@openstudio.fr>
2016-01-21 10:45:05 +0100

    first commit

---
+++ DemoTheliaNet.php
@@ -0,0 +1,25 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+
+namespace DemoTheliaNet;
+
+use Thelia\Module\BaseModule;
+
+/**
+ * Class DemoTheliaNet
+ * @package DemoTheliaNet
+ * @author Thomas Arnaud <tarnaud@openstudio.fr>
+ */
+class DemoTheliaNet extends BaseModule
+{
+    const DOMAIN_NAME = 'demothelianet';
+}
